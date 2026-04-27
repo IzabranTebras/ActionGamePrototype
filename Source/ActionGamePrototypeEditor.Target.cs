2a09517e815d4ec9c5594527a986d52d0344eee1
IzabranTebras <cristian.fernandezsua@gmail.com>
2026-04-27 17:28:05 +0200

    Updated Unreal Engine version to 5.7.4

--- Source/ActionGamePrototypeEditor.Target.cs
+++ Source/ActionGamePrototypeEditor.Target.cs
@@ -8,8 +8,8 @@ public class ActionGamePrototypeEditorTarget : TargetRules
 	public ActionGamePrototypeEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("ActionGamePrototype");
 	}
 }
